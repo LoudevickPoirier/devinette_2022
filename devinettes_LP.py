4ed4d33e8e1307a6a8a349c4322fbfe31c46b18d
LoudevickPoirier <loudevickpoirier@gmail.com>
2022-11-03 16:42:19 -0400

    Initial commit

---
+++ devinettes_LP.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    """
+    Fonction principale
+    """
+    print("LP: Jeu de devinette...")
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
